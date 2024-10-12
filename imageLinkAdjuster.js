a1aa32710aad36b268bf077e154a3c310475c0de
Andrew <andrewdoty1235@gmail.com>
2024-10-11 19:50:10 -0500

    Updating the files for image hosting.

---
+++ imageLinkAdjuster.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+function updateJsonFiles(directory) {
+  const files = fs.readdirSync(directory);
+
+  files.forEach(file => {
+    const filePath = path.join(directory, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      updateJsonFiles(filePath);
+    } else if (file.endsWith('.json')) {
+      let data = fs.readFileSync(filePath, 'utf8');
+      let json = JSON.parse(data);
+
+      // Recursively update cardimage URLs
+      function updateCardImages(obj) {
+        for (let key in obj) {
+          if (typeof obj[key] === 'object') {
+            updateCardImages(obj[key]);
+          } else if (key === 'cardimage' && obj[key].includes('lensdump')) {
+            const imageName = obj[key].split('/').pop();
+            obj[key] = `https://api.swarmada.wiki/images/${imageName}`;
+          }
+        }
+      }
+
+      updateCardImages(json);
+
+      fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
+    }
+  });
+}
+
+updateJsonFiles('./public/converted-json');
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const path = require('path');
 const shipRoutes = require('./routes/shipRoutes');
 const squadronRoutes = require('./routes/squadronRoutes');
 const upgradeRoutes = require('./routes/upgradeRoutes');
@@ -15,12 +16,21 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-app.use('/', baseRoutes)
+app.use('/', baseRoutes);
 
 app.use('/api/ships', shipRoutes);
 app.use('/api/squadrons', squadronRoutes);
 app.use('/api/upgrades', upgradeRoutes);
 app.use('/api/objectives', objectiveRoutes);
+
+// Add this new route for serving images
+app.use('/images', express.static(path.join(__dirname, 'images'), {
+  maxAge: '7d',
+  setHeaders: (res, path) => {
+    res.setHeader('Cache-Control', 'public, max-age=604800, immutable');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+  }
+}));
 
 app.use(errorHandler);
 
