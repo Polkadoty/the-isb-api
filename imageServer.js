f12142fcb5c53be249e41ed34a21af53a6c78983
Polkadoty <andrewdoty1235@gmail.com>
2024-10-12 14:16:13 -0500

    Updated the image server again.

--- imageServer.js
+++ imageServer.js
@@ -3,7 +3,10 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/images', express.static(path.join(__dirname, 'images'), {
+app.use('/images', (req, res, next) => {
+  console.log(`Attempting to serve image: ${req.url}`);
+  next();
+}, express.static(path.join(__dirname, 'images'), {
   maxAge: '7d',
   setHeaders: (res, path) => {
     res.setHeader('Cache-Control', 'public, max-age=604800, immutable');
@@ -13,6 +16,11 @@ app.use('/images', express.static(path.join(__dirname, 'images'), {
 
 app.get('/', (req, res) => {
   res.send('Image server is running');
+});
+
+app.use((req, res, next) => {
+  console.log(`404 - Not Found: ${req.method} ${req.url}`);
+  res.status(404).send('Not Found');
 });
 
 app.listen(port, () => {
