9cdf95e081f91e149b4d725c64b91fe087e23163
Polkadoty <andrewdoty1235@gmail.com>
2024-10-12 18:07:58 -0500

    Added shell script to restart pm2 instances.

---
+++ update_pm2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Stop all PM2 processes
+pm2 stop all
+
+# Delete all PM2 processes
+pm2 delete all
+
+# Start new PM2 instances
+pm2 start index.js --name "api-server" --node-args="--experimental-modules"
+pm2 start imageServer.js --name "image-server" --node-args="--experimental-modules"
+
+# Save the PM2 process list
+pm2 save
+
+# Display the status of PM2 processes
+pm2 status
+
+# Refresh nginx
+sudo systemctl restart nginx
